9da72a5ac812207b898f97d9e5b550b3efbd4476
vat99 <rrty65899@gmail.com>
2022-12-14 17:30:37 -0500

    refactor for pt2

--- day09/solution.py
+++ day09/solution.py
@@ -23,7 +23,6 @@ class Solution(abc.ABC):
     def main(self) -> int:
         pass
 
-class Part01(Solution):
     def change_state(self, direction: str, head_position: Tuple[int, int], tail_position: Tuple[int, int]) -> Tuple[Tuple[int,int], Tuple[int, int]]:
         """
             move `head_position` in `direction` by 1 step
@@ -47,13 +46,22 @@ class Part01(Solution):
                 head_position = (head_position[0], head_position[1]-1)
         if not self.check_position(head_position, tail_position):
             tail_position = old_head_position
-
         return head_position, tail_position
         
     def transform_instruction(self, instruction: str) -> Tuple[str, int]:
         instruction = instruction.split()
         return (instruction[0], int(instruction[1]))
 
+    def check_position(self, head_position: Tuple[int, int], tail_position: Tuple[int, int]) -> bool:
+        return abs(head_position[0]-tail_position[0]) <= 1 and abs(head_position[1]-tail_position[1]) <= 1
+
+    def check_diagonal(self, head_position: Tuple[int, int], tail_position: Tuple[int, int]) -> bool:
+        """
+        returns True if head_position 
+        """
+        return (head_position == (tail_position[0]+1, tail_position[1]+1)) or (head_position == (tail_position[0]-1, tail_position[1]+1)) or (head_position == (tail_position[0]+1, tail_position[1]-1)) or (head_position == (tail_position[0]-1, tail_position[1]-1))
+
+class Part01(Solution):
     def main(self, fname: str) -> int:
         # init
         head_position, tail_position = (0,0), (0,0)
@@ -68,15 +76,6 @@ class Part01(Solution):
                 head_position, tail_position = self.change_state(direction, head_position, tail_position)
                 visited_set.add(tail_position)
         return len(visited_set)
-    
-    def check_position(self, head_position: Tuple[int, int], tail_position: Tuple[int, int]) -> bool:
-        return abs(head_position[0]-tail_position[0]) <= 1 and abs(head_position[1]-tail_position[1]) <= 1
-
-    def check_diagonal(self, head_position: Tuple[int, int], tail_position: Tuple[int, int]) -> bool:
-        """
-        returns True if head_position 
-        """
-        return (head_position == (tail_position[0]+1, tail_position[1]+1)) or (head_position == (tail_position[0]-1, tail_position[1]+1)) or (head_position == (tail_position[0]+1, tail_position[1]-1)) or (head_position == (tail_position[0]-1, tail_position[1]-1))
 
 @timer_func
 def run(fname: str):
